72f6c0959a31b951aa608499f3f20ec7ba7aac86
georgecode <georgeczurlanis@gmail.com>
2018-11-13 19:21:54 -0800

    Add getgiffs component

---
+++ src/components/GetGifs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GetGifs extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Get gifs</h1>
+      </div>
+    );
+  }
+}
+
+export default GetGifs;

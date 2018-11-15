c1f617fbfc028a37a5787df8f9c4d6d45046c3ec
georgecode <georgeczurlanis@gmail.com>
2018-11-14 17:36:44 -0800

    Add ability to map multiple images in GifCon component

--- src/components/GetGifs.js
+++ src/components/GetGifs.js
@@ -6,13 +6,14 @@ export default (search) => {
 	//const corsHack = "http://cors-anywhere.herokuapp.com/"
 	return axios({
     method: "get",
-    url: "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=nSrqu5nPMLDZWmKICduK7XeVKj5tOJCU&limit=3"
+    url: "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=nSrqu5nPMLDZWmKICduK7XeVKj5tOJCU&limit=10"
 
   }).then(function(response) {
   	console.log("Responce from giphy worked",response.data.data[0].images.downsized_medium.url)
     //console.log('giphy RESPONCE',response.data.data[0])
     //return "testing"
-    return response.data.data[0].images.downsized_medium.url
+    return response.data.data
+    //return response.data.data[0].images.downsized_medium.url
     //return response.data.data[1];
   });
 }
--- src/components/GifCon.js
+++ src/components/GifCon.js
@@ -3,36 +3,51 @@ import loading from '../images/loading.gif';
 //import GetgifsTest from './GetgifsTest';
 import GetGifs from './GetGifs'
 
+
+let testStructure ={images:{downsized_medium:{url:loading}}}
+console.log(testStructure.images.downsized_medium.url)
+
 class GifCon extends Component {
 	constructor(props) {
     super(props);
     this.state = {
-      images:[loading,loading,loading],
+    	images:[{images:{downsized_medium:{url:loading}}}],
       testImage: loading
     }
   }
 
    componentDidMount() {
     GetGifs("serch xyzzz").then(value => {
+    	console.log("#@#@#@#@#@#@#@#@#@#@#@#@#@#", value[0])
+      this.setState({
+      	testImage:value[0].images.downsized_medium.url
+      });
+    });
+
+    GetGifs("serch xyzzz").then(value => {
     	//console.log("#@#@#@#@#@#@#@#@#@#@#@#@#@#", value)
       this.setState({
-      	testImage:value
+      	images:value
       });
     });
 
   }  
 
+//response.data.data[0].images.downsized_medium.url
   render() {
-
+  	console.log("image value", this.state.images)
   	//GetGifs("serch xyzzz")
-
+//   <img src={this.state.testImage} alt="test" />
     return (
       <div>
-      <img src={this.state.testImage} alt="test" />
+    	<img src={this.state.testImage} alt="test" />
         <h1> Gif cotain</h1>
                {this.state.images.map(el=>{
           return (
-            <img src={el} alt="test" />
+            //console.log("URL'S###",el.images.downsized_medium.url)
+            <img src={el.images.downsized_medium.url} alt="test" />
+        
+          	
          
             )
           })}
@@ -40,5 +55,10 @@ class GifCon extends Component {
     );
   }
 }
-
+//<img src={el.image.downsized_medium.url} alt="test" />
 export default GifCon;
+
+
+
+
+

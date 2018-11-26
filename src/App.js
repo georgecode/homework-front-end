0659cdf30cb66621585927ef65df395f3f816d3b
georgecode <georgeczurlanis@gmail.com>
2018-11-26 03:43:39 -0800

    Clean up console logs and comments

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import React, { Component } from 'react';
-//import Header from './components/Header';
 import SearchBar from './components/SearchBar';
-//import GifCon from './components/GifCon';
-//import loading from './images/loading.gif';
-import MosaicStyle from './components/MosaicStyle';
-//import Gallery from './components/Gallery';
 import { MuiThemeProvider } from "@material-ui/core/styles";
+//Theme is uppercase because github gives me problems if I change it
 import Theme from "./components/Theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { withTheme } from "@material-ui/core/styles";
-
-// const styles = theme => ({
-//   //testMui:{color:"red"}
-// })
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.theme = Theme();
-    //console.log("theme", this.theme);
-    // this.state = {}
   }  
   render() {
     return (
       <MuiThemeProvider theme={this.theme}>
-        <CssBaseline />
-        
-        <SearchBar />
-                            
+        <CssBaseline />     
+        <SearchBar />                      
       </MuiThemeProvider>
     );
   }
 }
-//<MosaicStyle />
 
-//<SearchBar />
-//export default App;
-//export default withStyles(styles)(App)
 export default withTheme()(App);
 
 
